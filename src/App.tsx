1b93417eff58139f6fd719791745ad3823dc5227
Sahan Pasindu <sahanperera1997@gmail.com>
2020-10-13 09:11:50 +0530

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import MyRouter from "./routes/routes";
 import "./App.css";
-import HomePage from "./pages/HomePage/home_page";
 
 export default function App() {
   return (
     <div>
-      <HomePage/>
+      <MyRouter/>
     </div>
   );
 }
