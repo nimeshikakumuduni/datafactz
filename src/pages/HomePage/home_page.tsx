acf4a1131dcc376110ea3dda9606ba697b108d3c
nimeshika <nimeshikakumuduni@gmail.com>
2020-10-19 10:37:31 +0530

    ok

--- src/pages/HomePage/home_page.tsx
+++ src/pages/HomePage/home_page.tsx
@@ -1,57 +1,166 @@
 import React from "react";
 import useStyles from "./home_page_styles";
-import { List, ListItem} from "@material-ui/core";
+import { List, ListItem } from "@material-ui/core";
 import MyAppBar from "../../components/app_bar";
-import LogoArea from "../../components/logoArea/logo_area";
-
 
 function HomePage() {
   const classes = useStyles();
 
   return (
-      <div className={classes.outer}>
-        <div className={classes.appBar}>
-          <MyAppBar />
-          <div className={classes.cont1}>
-            <img
-              className={classes.img2}
-              alt="before"
-              src="https://datafactz.com/wp-content/themes/datafactz/dist/images/before.jpg"
-              id="beforeimage27895723"
-            />
-          </div>
-          <div className={classes.homecontent}>
-            <h3 className={classes.heading} id="hero-tabs-heading">
-              Unparalleled Business Analytics For Data-Driven Solutions
-            </h3>
-          </div>
-          <div>
-          <List className={classes.tab}>
-            <ListItem className={classes.one}>
-              <h5 className={classes.title1}>Supply Chain Analytics</h5>
-            </ListItem>
-            <ListItem>
-              <h5 className={classes.title1}>Customer Analytics</h5>
-            </ListItem>
-            <ListItem>
-              <h5 className={classes.title1}>Risk Analytics</h5>
-            </ListItem>
-            <ListItem>
-              <h5 className={classes.title1}>Marketing Analytics</h5>
-            </ListItem>
-            <ListItem>
-              <h5 className={classes.title1}>Social Media Analytics</h5>
-            </ListItem>
-            <ListItem>
-              <h5 className={classes.title1}>Real Time Analytics</h5>
-            </ListItem>
-          </List></div>
+    <div className={classes.outer}>
+      <div className={classes.appBar}>
+        <MyAppBar />
+        <div className={classes.cont1}>
+          <img
+            className={classes.img2}
+            alt="before"
+            src="https://datafactz.com/wp-content/themes/datafactz/dist/images/before.jpg"
+            id="beforeimage27895723"
+          />
+        </div>
+        <div className={classes.homecontent}>
+          <h3 className={classes.heading} id="hero-tabs-heading">
+            Unparalleled Business Analytics For Data-Driven Solutions
+          </h3>
+        </div>
+        <List className={classes.tab}>
+          <ListItem className={classes.one}>
+            <h5 className={classes.title1}>Supply Chain Analytics</h5>
+          </ListItem>
+          <ListItem>
+            <h5 className={classes.title1}>Customer Analytics</h5>
+          </ListItem>
+          <ListItem>
+            <h5 className={classes.title1}>Risk Analytics</h5>
+          </ListItem>
+          <ListItem>
+            <h5 className={classes.title1}>Marketing Analytics</h5>
+          </ListItem>
+          <ListItem>
+            <h5 className={classes.title1}>Social Media Analytics</h5>
+          </ListItem>
+          <ListItem>
+            <h5 className={classes.title1}>Real Time Analytics</h5>
+          </ListItem>
+        </List>
 
-          <div className={classes.container}>
-          <LogoArea/>
+        <div className={classes.container}>
+          <div id="myCarousel" className={classes.slide}>
+            <div className={classes.inner}>
+              <div>
+               
+                <div className={classes.image}>
+                  <img
+                    src="https://datafactz.com/wp-content/themes/datafactz/dist/images/amazon.png"
+                    alt="amazon"
+                  />
+                </div>
+                <div className={classes.image}>
+                  <img
+                    src="https://datafactz.com/wp-content/themes/datafactz/dist/images/AAA.png"
+                    alt="AAA"
+                  />
+                </div>
+                <div className={classes.image}>
+                  <img
+                    src="https://datafactz.com/wp-content/themes/datafactz/dist/images/centene.png"
+                    alt="centene"
+                  />
+                </div>
+                <div className={classes.image}>
+                  <img
+                    src="https://datafactz.com/wp-content/themes/datafactz/dist/images/pacific-life.png"
+                    alt="pacific-life"
+                  />
+                </div>
+                <div className={classes.image}>
+                  <img
+                    src="https://datafactz.com/wp-content/themes/datafactz/dist/images/pch.png"
+                    alt="pch"
+                  />
+                </div>
+                <div className={classes.image}>
+                  <img
+                    src="https://datafactz.com/wp-content/themes/datafactz/dist/images/starbucks.png"
+                    alt="starbucks"
+                  />
+                </div>
+                <div className={classes.image}>
+                  <img
+                    src="https://datafactz.com/wp-content/themes/datafactz/dist/images/coca-cola.png"
+                    alt="coca-cola"
+                  />
+                </div>
+              </div>
+              <div>
+                <div className={classes.image}>
+                  <img
+                    src="https://datafactz.com/wp-content/themes/datafactz/dist/images/meijer.png"
+                    alt="meijer"
+                  />
+                </div>
+                <div className={classes.image}>
+                  <img
+                    src="https://datafactz.com/wp-content/themes/datafactz/dist/images/sears.png"
+                    alt="sears"
+                  />
+                </div>
+                <div className={classes.image}>
+                  <img
+                    src="https://datafactz.com/wp-content/themes/datafactz/dist/images/cable-vision.png"
+                    alt="cable-vision"
+                  />
+                </div>
+                <div className={classes.image}>
+                  <img
+                    src="https://datafactz.com/wp-content/themes/datafactz/dist/images/maximus.png"
+                    alt="maximus"
+                  />
+                </div>
+                <div className={classes.image}>
+                  <img
+                    src="https://datafactz.com/wp-content/themes/datafactz/dist/images/cheesecake-factory.png"
+                    alt="cheesecake-factory"
+                  />
+                </div>
+              </div>
+              <div>
+                <div className={classes.image}>
+                  <img
+                    src="https://datafactz.com/wp-content/themes/datafactz/dist/images/image11.png"
+                    alt="rockspace"
+                  />
+                </div>
+                <div className={classes.image}>
+                  <img
+                    src="https://datafactz.com/wp-content/themes/datafactz/dist/images/pch.png"
+                    alt="pch"
+                  />
+                </div>
+                <div className={classes.image}>
+                  <img
+                    src="https://datafactz.com/wp-content/themes/datafactz/dist/images/starbucks.png"
+                    alt="starbucks"
+                  />
+                </div>
+                <div className={classes.image}>
+                  <img
+                    src="https://datafactz.com/wp-content/themes/datafactz/dist/images/coca-cola.png"
+                    alt="coca-cola"
+                  />
+                </div>
+                <div className={classes.image}>
+                  <img
+                    src="https://datafactz.com/wp-content/themes/datafactz/dist/images/gap.png"
+                    alt="gap"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
--- src/pages/HomePage/home_page_styles.tsx
+++ src/pages/HomePage/home_page_styles.tsx
@@ -4,12 +4,13 @@ const useStyles = makeStyles({
   outer: {
     width: "100%",
     height: "100%",
-    overflowX: "hidden",
+    overflow: "hidden",
     position: "absolute",
   },
   appBar: {
     position: "fixed",
     width: "100%",
+    overflow: "scroll",
   },
 
   text1: {
@@ -46,16 +47,9 @@ const useStyles = makeStyles({
     fontWeight: 500,
     color: "meroon",
   },
-  serviceBtn: {
-    backgroundColor: "#9a9a9a30",
-    margin: "5px",
-    width: "300px",
-    borderRadius: "5px",
-    opacity: "2",
-  },
+ 
   homecontent: {
     width: "100%",
-    paddingLeft: "150px",
     backgroundColor: "#101820",
     marginTop: "35.5%",
   },
@@ -102,10 +96,32 @@ const useStyles = makeStyles({
     cursor: "pointer",
   },
   container: {
-    width: "100%",
-    height: "100%",
-    marginTop: "10%",
+    width: "100%" !,
+    height: "40%",
     color: "blue",
+    marginRight: "auto",
+    marginLeft: "auto",
+    backgroundColor: "#37a1af",
+
+  },
+  slide: {
+    position: "relative",
+    height: "200px"
+  },
+  inner: {
+    position: "relative",
+    width: "100%",
+    overflow: "hidden",
+  },
+  image: {
+    display: "inline-block",
+    width: "10%",
+    height: "10%",
+    textAlign: "center",
+    marginBottom: "3px",
+    marginTop: "3px",
+    marginLeft: "30px",
+    marginRight: "30px",
   },
 });
 
